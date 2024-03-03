91aa2fa7a1d4f213a4d26ebc4f061b41f9d84949
kami1983 <kami@cancanyou.com>
2024-03-03 13:42:57 +0800

    Out put request information.

---
+++ routes/tg_test.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  
+  console.log('-------- Get --------');
+  console.log(req.query);
+
+  console.log('-------- Post --------');
+  console.log(req.body);
+
+  res.send('Tg test page');
+});
+
+module.exports = router;
