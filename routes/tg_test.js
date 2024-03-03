bbbce3f95299af12bf39f0f43b693087dbc0d65d
kami1983 <kami@cancanyou.com>
2024-03-03 14:12:25 +0800

    Add post

--- routes/tg_test.js
+++ routes/tg_test.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.all('/', function(req, res, next) {
   
   console.log('-------- Get --------');
   console.log(req.query);
